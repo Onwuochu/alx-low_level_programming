3f49dac57583b1346cbe19791206a024748080ac
Onwuochu <cordeliaekpenukpang@gmail.com>
2023-06-15 22:31:20 -0700

    the puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
-* main - A program that prints a line with a puts function
-* Return: 0 (Success)
-*/
+ * main - A program that prints with a puts function
+ * Return: 0 (Success)
+ */
 int main(void)
-{
-puts("\"programming is like building a multilingual puzzle");
+{puts("\"programming is like building a multilingual puzzle");
 return (0);
 }
