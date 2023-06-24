23e3bd71faa27d046c789be2c7191bbca45da7d5
Onwuochu <cordeliaekpenukpang@gmail.com>
2023-06-24 12:14:51 -0700

    Updated all

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - A program that prints with a puts function
- * Return: 0 (Success)
+ * main - Entry point
+ *
+ * Return: anytime 0 (Success)
  */
 int main(void)
-{puts("\"programming is like building a multilingual puzzle");
-return (0);
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0)
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 /**
- * main - A program that prints various size of types of computer function
- * Return: 0 (Success)
+ * main - A program that prints the size of various computer types
+ * Return 0
  */
+
 int main(void)
 {
-char a;
-int b;
-long int c;
-lon long int d;
-float f;
-printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
-Return (0);
-}
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of char: %lu byte(s)\n", (unsigned longcd)
